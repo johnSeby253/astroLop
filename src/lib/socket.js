810ca89889acee4808a1358abb48b5af728e9784
John Seby <johnseby253@gmail.com>
2026-04-13 17:48:37 +0530

    first commit15

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -3,22 +3,22 @@ import { io } from "socket.io-client";
 
 let socket;
 
-export const getSocket = () => {
-  if (!socket) {
-    socket = io("http://175.1.1.177:5000", {
-      withCredentials: true,
-      transports: ["websocket"],
-    });
-  }
-  return socket;
-};
-
 // export const getSocket = () => {
 //   if (!socket) {
-//     socket = io("https://astrolop-backend.onrender.com", {
+//     socket = io("http://175.1.1.177:5000", {
 //       withCredentials: true,
 //       transports: ["websocket"],
 //     });
 //   }
 //   return socket;
 // };
+
+export const getSocket = () => {
+  if (!socket) {
+    socket = io("https://astrolop-backend.onrender.com", {
+      withCredentials: true,
+      transports: ["websocket"],
+    });
+  }
+  return socket;
+};
